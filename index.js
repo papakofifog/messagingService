603afb203d2ab49b7938b9e14d997d4679648015
paakofifog <paakofi275@gmail.com>
2023-10-26 01:58:45 +0000

    initial commit

--- index.js
+++ index.js
@@ -18,31 +18,25 @@ const sio = Server(server, {
 
 
 function sendMessageToReceiver(clientSocket,data){
-    console.log(clientSocket)
+    //console.log(clientSocket)
     clients.get(clientSocket).emit('receiveMessage', data)
 }
 
-function sendTypingStatus(clientSocket){
-    clientSocket.get(clientSocket).emit('typing', 'typing...');
+function sendTypingStatus(clientSocket, status){
+    clientSocket.get(clientSocket).emit('typingStatus', false);
 }
 
 sio.on("connection", function(socket){
-    
-    
     socket.on('setUserId', async (msg)=> {
-
         if(msg !=""){
             clients.set(msg, socket) 
         }
-        
-        
     })
     // When a client sends a message
     socket.on('sendMessage', async (data)=> {
-        let clientSocket= data.receipientId
+        let clientSocket= data.recipientId
         if (clients.has(clientSocket)) {
             //save chat 
-
             console.log(data)
             sendMessageToReceiver(clientSocket,data);
         }else{
@@ -52,14 +46,16 @@ sio.on("connection", function(socket){
 
     });
 
-    socket.on('typing', (receipientId)=>{
-        let clientSocket=receipientId;
-        if (clients.has(clientSocket)){
-            
-            sendTypingStatus(receipientId);
-        }else{
-            clients.set(receipientId,socket);
-            sendTypingStatus(receipientId);
+    socket.on('onTyping', (activeUserId)=>{
+        console.log("hello")
+        if (clients.has(activeUserId)){
+            sendTypingStatus(activeUserId, true);
+        }
+    })
+
+    socket.on ('stopedTyping', (activeUserId)=>{
+        if (clients.has(activeUserId)){
+            sendTypingStatus(activeUserId, false);
         }
     })
 });
